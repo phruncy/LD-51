f05a8201ab674f3c3729f4d355914b2b0245f5ce
Franziska Schneider <franziska.schneider@me.com>
2022-10-02 19:40:48 +0200

    Adds listener for timer to Agent Spawner

--- ludum-dare-51/Assets/Scripts/SingleHitDestructor.cs
+++ ludum-dare-51/Assets/Scripts/SingleHitDestructor.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +6,19 @@ namespace LD51
 {
     public class SingleHitDestructor : MonoBehaviour
     {
+        public event Action OnHit;
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.gameObject.tag == "Node")
                 InitializeDestruction();
 
+
         }
 
         private void InitializeDestruction()
         {
+            OnHit?.Invoke();
             Destroy(transform.gameObject);
         }
     }
